940515a0cfdf2ec49fc896e5cecb386be36da21a
Arpit Verma <arpitv970@gmail.com>
2023-11-02 13:58:00 +0530

    added Privacy page

---
+++ components/Privacy.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Privacy = () => {
+  return (
+    <section className='docs'>
+      <h1 className='docs-header'>Privacy Policy</h1>
+      <section className='docs-content'>
+      </section>
+    </section>
+  )
+}
+
+export default Privacy
